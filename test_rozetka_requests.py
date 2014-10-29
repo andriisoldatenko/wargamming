6d666b5f1e50e6912b04f305d27d45aef6738d4f
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2014-10-29 08:54:46 +0200

    Refactor test

--- test_rozetka_requests.py
+++ test_rozetka_requests.py
@@ -7,30 +7,49 @@ import unittest
 from lxml import html
 
 
+class Rozetka(object):
+
+    def __init__(self, url, text=''):
+        self.text = text
+        self.url = url
+        self.request = requests.get(url, params={'text': text})
+
+    def json_response(self):
+        return self.request.json()
+
+    def page_source(self):
+        return self.request.text
+
+    def html_tree(self):
+        pass
+
+
 class TestRozetkaSearch(unittest.TestCase):
 
     def setUp(self):
-        _text = "D-Link DIR-826L"
-        params = {'text': _text}
-        page = requests.get('http://rozetka.com.ua/search/', params=params)
-        self.page_text = page.text
-        self.tree = html.fromstring(self.page_text)
+        page = Rozetka('http://rozetka.com.ua/search/', text="D-Link DIR-826L")
+        self.tree = html.fromstring(page.page_source())
 
     def test_search_d_link_price(self):
-        price = self.tree.xpath('//div[@class="g-i-list-price-uah"]/text()')[0].strip()
-        price_sign = self.tree.xpath('//span[@class="g-i-list-price-uah-sign"]/text()')[0].strip()
-        self.assertEqual('%s %s' % (price, price_sign), u"952 грн.")
+        price = self.tree.find_class("g-i-list-price-uah")[0].text
+        price_sign = self.tree.find_class("g-i-list-price-uah-sign")[0].text
+
+        self.assertEqual('%s%s' % (price, price_sign), u"952\u2009грн.")
 
     def test_search_d_link_status(self):
-        status = self.tree.xpath('//div[@class="g-i-list-status unavailable"]/text()')[0].strip()
+        status = self.tree.find_class("g-i-list-status unavailable")[0].text.strip()
+
         self.assertEqual(status, u"Нет в наличии")
 
     def test_search_d_link_usd_value(self):
-        find_goods_id = re.search(r'id:\ \d+', self.page_text)
-        self.goods_id = find_goods_id.group().split()[1]
-        page = requests.get('http://rg.rozetka.com.ua/recent_goods_sync/action=getRecentGoodsForStorage;goods_ids=%s/' 
-                            % self.goods_id)
-        currency = page.json().get('content')[0].get('price_usd')
+        find_goods_id = re.search(r'id:\ \d+', self.page.page_source())
+        goods_id = find_goods_id.group().split()[1]
+        rest_url = "http://rg.rozetka.com.ua/recent_goods_sync/" \
+                   "action=getRecentGoodsForStorage;goods_ids=%s/" % goods_id
+
+        page = Rozetka(rest_url)
+        currency = page.json_response().get('content')[0].get('price_usd')
+
         self.assertEqual(float(currency), 68.50)
 
 
